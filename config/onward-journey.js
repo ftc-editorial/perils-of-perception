174e2cd11e2e9d57bb9f0011106ea1ee7bee0d4c
Martin Stabe <martinstabe@gmail.com>
2016-12-13 18:13:41 +0000

    Initial config

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'list/graphics' },
+    { rows: 1, list: 'thing/MGY2ZTQ3MTYtYjJiNS00ODVhLTlkYTktNzZlNzc3YTcxOWYy-QnJhbmRz' },
     { rows: 1, list: 'list/highlights' },
   ],
 });
