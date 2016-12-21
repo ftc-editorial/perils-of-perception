02bbd8a35f8cd9eabb155d13e48061b3bcda420b
wangyichen1064431086 <1064431086@qq.com>
2016-12-21 19:24:23 +0800

    understand how to get the question data

--- client/index.js
+++ client/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import Question from './components/question';
 import Overlay from './components/overlay';
 import Share from 'ftc-share';
-const shareInstance = new Share(document.querySelector('[data-o-component=o-share]'));
+import {data} from '../config/data/china.js';//import只能放在最外层
+
+const shareInstance = new Share(document.querySelector('[data-o-component=o-share]'));//产生社交网络分享按钮
 
 const endpoint = 'https://ft-ig-answer-api.herokuapp.com/api/v1';
 
-class App extends Component {
+class App extends Component {//类App继承了react的类Component
   constructor(props) {
-    super(props);
+    super(props);//调用父类Component的构造方法
     const { questions } = props;
 
     this.state = {
@@ -23,30 +25,57 @@ class App extends Component {
       chooseQuestions: true,
       country: null,
     };
-    this.setQuestions = this.setQuestions.bind(this);
+    this.setQuestions = this.setQuestions.bind(this);//将该类上的方法绑定在该类上
     this.updateProgress = this.updateProgress.bind(this);
     this.updateScore = this.updateScore.bind(this);
   }
 
   setQuestions(value) {
     console.log(`Country selected: ${value}`);
-    const key = value.toLowerCase().replace(/\s/g, '-');
-    const data = `https://ft-ig-content-prod.s3.amazonaws.com/v1/ft-interactive/answer-api/2/2__perils-of-perception-survey-2016__${key}.json`;
-    // fetch(`${endpoint}/project/1?aggregate=true&key=Country&value=${value}`)
-    fetch(data)
-      .then(res => res.json())
-      .then(({ questions }) => this.setState({
+    const key = value.toLowerCase().replace(/\s/g, '-');//将value值都化为小写，并将其中的空白符都替换为'-'
+
+    //const data = `https://ft-ig-content-prod.s3.amazonaws.com/v1/ft-interactive/answer-api/2/2__perils-of-perception-survey-2016__${key}.json`;//这里是下数据的地址
+
+    //fetch(data)
+    /* 知识补充：
+     fetch() 方法用于发起获取资源的请求，默认方法是GET。参数data是请求的URL。它返回一个 promise，这个 promise 会在请求响应后被 resolve，并传回 Response 对象。
+     */
+      //.then(res => res.json())
+      /* 知识补充：
+        response对象的方法.json():返回一个promise,resolve得到一个包含json数据的对象字面量
+      */
+     // .then(({ questions }) => this.setState({
+        /* 知识补充：
+         * 此处then()的链式调用，这里的参数{questions}就是前一个then的return结果??待确认
+         * setState(nextState, callback)：React.Component类的方法，
+        */
+
+        //questionsLoaded: true,
+        //questions,
+        //country: value,
+      //}));
+      /*
+      ({questions}) => this.setState({
         questionsLoaded: true,
-        questions,
+        data,
         country: value,
-      }));
+      });
+      */
+      this.setState({
+        questionsLoaded: true,
+        ...data,
+        country: value,
+      })
   }
 
   updateProgress(n) {
+    /* 用于更进做到哪一题了，
+     * @param n,做到的题号数
+    */
     if (n <= this.state.questions.length - 1) {
       this.setState({ activeQuestion: n });
     } else {
-      this.setState({
+      this.setState({ 
         activeQuestion: null,
         complete: true,
       });
@@ -65,10 +94,10 @@ class App extends Component {
       <p><strong>问卷加载中...</strong></p>;
 
     const questions = this.state.questions
-      .filter(question => question.answer !== '')
-      .sort((a, b) => Number(a.meta.qid.slice(1)) - Number(b.meta.qid.slice(1)))
-      .slice(2)
-      .map((question, i) =>
+      .filter(question => question.answer !== '')//将questions数组返回其每个数组项的answer属性不为''的数组项
+      .sort((a, b) => Number(a.meta.qid.slice(1)) - Number(b.meta.qid.slice(1)))//升序排列（应该是按题号升序排列）
+      .slice(2)//从索引为2开始提取这个数组
+      .map((question, i) =>//参数question:数组项，参数i:数组索引
         <Question
           key={question.meta.qid}
           questionId={question.id}
@@ -101,15 +130,18 @@ class App extends Component {
       <div>
         <link rel="stylesheet" href="https://build.origami.ft.com/v2/bundles/css?modules=o-buttons@^4.4.1" /> 
 
-        {loadStatus}
+        {loadStatus} 
 
         {questions}
 
         {results}
 
-        {chooseQuestions}
+        {chooseQuestions} 
       </div>
-    );
+    );//loadStatus 为null或 "<p><strong>问卷加载中...<\/strong><\/p>" 
+      //question  为一个数组，每个数组项为"<Question ... \/>"
+      // results 为null或 "<div><p>Your score: {his.state.score}<\/p><\/div>"
+      // chooseQuestions=为null或 "<Overlay setQuestions={this.setQuestions} \/>"
   }
 }
 
@@ -119,4 +151,4 @@ App.propTypes = {
 
 
 
-ReactDOM.render(<App />, document.querySelector('#react-container'));
+ReactDOM.render(<App />, document.querySelector('#react-container')); //对'#react-container'处元素内部进行填充
